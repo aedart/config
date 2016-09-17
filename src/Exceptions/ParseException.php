35a9316f450d3efa5c73d66320339bf9b5a018ca
Alin Eugen Deac <aedart@gmail.com>
2016-09-17 17:24:39 +0200

    Add parse exception

---
+++ src/Exceptions/ParseException.php
@@ -0,0 +1,15 @@
+<?php
+namespace Aedart\Config\Exceptions;
+
+use RuntimeException;
+
+/**
+ * Parse Exception
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Config\Exceptions
+ */
+class ParseException extends RuntimeException
+{
+
+}
