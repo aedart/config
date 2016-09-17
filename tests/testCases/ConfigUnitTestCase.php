d456e372f4a8ba164238eff46e08933eac5572bb
Alin Eugen Deac <aedart@gmail.com>
2016-09-17 17:54:27 +0200

    Add unit test-case

---
+++ tests/testCases/ConfigUnitTestCase.php
@@ -0,0 +1,13 @@
+<?php
+
+use Aedart\Testing\TestCases\Unit\UnitTestCase;
+
+/**
+ * Config Unit Test-Case
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ */
+abstract class ConfigUnitTestCase extends UnitTestCase
+{
+
+}
